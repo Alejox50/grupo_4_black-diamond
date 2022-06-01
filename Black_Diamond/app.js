7c09d9a47cc1301b7c97ed9a562590eea62c3916
JuanPablo <mljuanpablo@gmail.com>
2022-06-01 17:14:52 -0500

    Sprint 2

---
+++ Black_Diamond/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(3001, () =>
+console.log('Servidor corriendo en el puerto 3001')
+);
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html"))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+});
