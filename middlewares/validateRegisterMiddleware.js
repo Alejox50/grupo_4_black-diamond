a86151b478070fe15e50db9199a42ad5609bf3d9
Alejox50 <alejorodasquintero@gmail.com>
2022-09-20 23:08:53 -0500

    old

--- middlewares/validateRegisterMiddleware.js
+++ middlewares/validateRegisterMiddleware.js
@@ -3,13 +3,18 @@ const { check: body } = require('express-validator');
 
 
 module.exports = [
-    body('nombres').notEmpty().withMessage('Tienes que escribir un nombre'),
-    body('apellidos').notEmpty().withMessage('Tienes que escribir un apellido'),
+    body('nombres')
+        .notEmpty().withMessage('Tienes que escribir un nombre'),
+    body('apellidos')
+        .notEmpty().withMessage('Tienes que escribir un apellido'),
     body('correo')
         .notEmpty().withMessage('Tienes que escribir un correo electronico').bail()
         .isEmail().withMessage('Debes escribir un formato de correo valido'),
-    body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
-    body('imagenUsuario').custom((value, { req }) =>  {
+    body('password')
+        .notEmpty().withMessage('Tienes que escribir una contraseña')
+        .isLength({ min:8 }).withMessage('La contraseña debe tener minimo 8 caracteres'),
+    body('imagenUsuario')
+        .custom((value, { req }) =>  {
         let file = req.file;
         let acceptedExtensions = ['.jpg', '.png', '.gif'];
         if (!file) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('../controllers/userController.js');
 const upload = require('../middlewares/multerMiddleware');
+const validateRegisterMiddleware = require('../middlewares/validateRegisterMiddleware');
 
 const validations = require('../middlewares/validateRegisterMiddleware')
 //const { dirname } = require('path');
