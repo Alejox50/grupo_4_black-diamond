b33c91282ce36585eb414fe11f3784980eba88e1
jmarulandabotero <you@example.com>
2022-11-08 10:14:30 -0500

    haciendo cambios en la carpeta app.js

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ const express = require("express");
 const app = express();
 const rutasUser = require('./routes/userRoutes');
 const rutasProduct = require('./routes/product.js');
-const session = require('express-session');
 const db = require('./database/models/index')
-
+const save = require('./database/setup');
+const cors = require('cors')
 
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
+var corsOptions = {
+    origin: ['http://localhost:3000','*'],
+}
 
+app.use(cors(corsOptions))
 
 app.use(bodyParser.json());
 app.use("/", rutasUser);
 app.use("/", rutasProduct);
 
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync({force: true}).then(async() => {
     console.log('conectando')
+    await save();
 });
+
+
 
 app.listen(process.env.PORT || 3001, function() {
     console.log('Servidor corriendo en el puerto 3001')
