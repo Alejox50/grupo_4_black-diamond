09d189bd7fb3e0d582dfbceef1553d440411d50a
jmarulandabotero <you@example.com>
2022-11-08 10:33:09 -0500

    haciendo  unos arreglos en el arcivo de userroutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const userController = require('./../controllers/userController');
-const upload = require('../middlewares/multerMiddleware');
-const validateRegister = require('../middlewares/validateRegisterMiddleware');
-const validateLogin = require('../middlewares/validateLoginMiddleware');
+const router = express.Router();
+const multer = require('multer')
+const { uuid } = require('uuidv4');
+
 
 //const { dirname } = require('path');
-const router = express.Router();
-const {verifyContentLogIn} = require('../middlewares/validateLoginMiddleware')
-const {verifyContentRegister} = require('../middlewares/validateRegisterMiddleware')
-//router.get("/login", userController.login);
+const { verifyContentLogIn } = require('../middlewares/validateLoginMiddleware')
+const { verifyContentRegister } = require('../middlewares/validateRegisterMiddleware')
+const userController = require('./../controllers/userController');
 
 //router.post("/register",validateRegister, upload.single("imagenUsuario") ,userController.save);
 
@@ -17,7 +16,23 @@ const {verifyContentRegister} = require('../middlewares/validateRegisterMiddlewa
 //router.post('/login', validateLogin, userController.processLogin);
 //router.post('/register', upload.single('imagenUsuario'), validations, userController.processRegister);
 //router.post('/',validateRegister, userController.processRegister)
-router.post("/signup", verifyContentRegister, userController.save )
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads/avatars')
+    },
+    filename: function (req, file, cb) {
+        var extension = file.originalname.split('.')[1]
+        var fileName = `${uuid()}.${extension}`
+        cb(null, fileName)
+    }
+})
+
+const upload = multer({ storage: storage })
+router.post('/uploadAvatar/:id', upload.single('uploaded_file'), userController.setImage);
+
+router.post("/signup", verifyContentRegister, userController.save)
 router.post('/login', verifyContentLogIn, userController.processLogin);
+router.get('/avatar/:id', userController.getImage);
+router.get('/getuserinfo', userController.getUser);
 
 module.exports = router;
