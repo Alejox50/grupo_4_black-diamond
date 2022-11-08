9ddb1d27375f1a40204b760dc3170bdd5d3f91e2
jmarulandabotero <you@example.com>
2022-11-08 10:34:26 -0500

    borrando unas lineas que estaban comentadas en el archivo de userroutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,19 +3,10 @@ const router = express.Router();
 const multer = require('multer')
 const { uuid } = require('uuidv4');
 
-
-//const { dirname } = require('path');
 const { verifyContentLogIn } = require('../middlewares/validateLoginMiddleware')
 const { verifyContentRegister } = require('../middlewares/validateRegisterMiddleware')
 const userController = require('./../controllers/userController');
 
-//router.post("/register",validateRegister, upload.single("imagenUsuario") ,userController.save);
-
-//router.get('/register',userController.register)
-
-//router.post('/login', validateLogin, userController.processLogin);
-//router.post('/register', upload.single('imagenUsuario'), validations, userController.processRegister);
-//router.post('/',validateRegister, userController.processRegister)
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/uploads/avatars')
